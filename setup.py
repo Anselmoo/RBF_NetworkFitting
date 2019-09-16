2c38bdd9b63ce62eb0a2ba6965779bba4cd862cc
Anselmoo <Anselm.Hahn@gmail.com>
2019-09-16 15:48:55 +0200

    Adding the setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+	name='RRBF_NetworkFitting',
+	version='0.5',
+	packages=['RBFN', 'test', 'examples'],
+	url='https://github.com/Anselmoo/RRBF_NetworkFitting',
+	license='MIT',
+	author='Anselm Hahn',
+	author_email='Anselm.Hahn@gmail.com',
+	description='Radial basis function network with genetic optimizer'
+)
